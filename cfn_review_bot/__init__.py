bfed0ec6bf6ee824e1b6fdca6484bb05a045900f
João Abecasis <joao@abecasis.name>
2019-04-28 16:00:08 +0200

    Document versioning scheme
    No code changes to note:
    
    - definitions in _version moved around
    - cosmetic changes to how _version.VersionInfo is defined
    - initial underscore dropped from _version._get_version_info() method
      name

--- cfn_review_bot/__init__.py
+++ cfn_review_bot/__init__.py
@@ -1,5 +1,5 @@
 from . import _version
 
 
-__version_info__ = _version._get_version_info()
+__version_info__ = _version.get_version_info()
 __version__ = __version_info__.package_version
--- cfn_review_bot/_version.py
+++ cfn_review_bot/_version.py
@@ -1,3 +1,35 @@
+'''
+Automated package versioning from source control.
+
+Because life is too short to fret over releases and version numbers, every
+revision in source control can, potentially, be a release. This module uses the
+revision's timestamp as the package version.
+
+As release numbers are generated automatically from version control, the
+variable `EPOCH` may be incremented to signal major (backwards incompatible)
+changes to the package. (In PEP 440, the epoch is specified as a way to support
+changing a package's versioning scheme. We abuse it here as a replacement for
+major version numbers in semantic versioning.)
+
+The class `VersionInfo` is a named tuple `(epoch, version, git_revision)`, that
+provides the computed property `package_version` as a string.
+
+The function `get_version_info()` checks for the presense of the file
+`package-version.json` in the package, and returns version information from
+there, if it is available. If the file is missing, source control (i.e., git) is
+queried, instead.
+
+Packaging scripts should call `prepare_version_info_for_package()` to query
+source control for version information, and generate `package-version.json`.
+This file should be included in released packages.
+
+Note: as the source control system may not enforce a monotonically increasing
+revision timestamp, care should be taken to avoid a regression of the package
+version. When using `git`, a development model based on feature branches where a
+merge commit is created at the time a feature is accepted into the main branch
+can provide this invariant.
+'''
+
 import collections
 import datetime
 import json
@@ -11,31 +43,12 @@ BASEDIR = os.path.dirname(__file__)
 PACKAGE_VERSION_FILE = os.path.join(BASEDIR, 'package-version.json')
 
 
-VersionInfo = collections.namedtuple(
-  'VersionInfo', ['epoch', 'version', 'git_revision'])
+class VersionInfo(
+  collections.namedtuple('VersionInfo', ['epoch', 'version', 'git_revision'])):
 
-VersionInfo.package_version = property(
-  lambda v: '{v.epoch}!{v.version}'.format(v=v))
-
-
-def prepare_version_info_for_package():
-  vi = _get_version_info_from_git()
-  with open(PACKAGE_VERSION_FILE, mode='w') as f:
-    json.dump(vi._asdict(), f, ensure_ascii=True, indent=2, sort_keys=True)
-  return vi
-
-
-def _get_version_info_from_package():
-  with open(PACKAGE_VERSION_FILE) as f:
-    data = json.load(f)
-  return VersionInfo(**data)
-
-
-def _get_git_timestamp():
-  git_output = subprocess.check_output(
-    'git log -1 --date=unix --format=format:%cd'.split(),
-    stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
-  return datetime.datetime.utcfromtimestamp(int(git_output))
+  @property
+  def package_version(self):
+    return '{s.epoch}!{s.version}'.format(s=self)
 
 
 def _get_git_revision():
@@ -45,6 +58,21 @@ def _get_git_revision():
   return git_output.strip().decode()
 
 
+def _get_git_timestamp():
+  git_output = subprocess.check_output(
+    'git log -1 --date=unix --format=format:%cd'.split(),
+    stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
+  return datetime.datetime.utcfromtimestamp(int(git_output))
+
+
+def get_version_info():
+  try:
+    return _get_version_info_from_package()
+  except FileNotFoundError:
+    pass
+  return _get_version_info_from_git()
+
+
 def _get_version_info_from_git():
   return VersionInfo(
     epoch=EPOCH,
@@ -52,10 +80,14 @@ def _get_version_info_from_git():
     git_revision=_get_git_revision())
 
 
-def _get_version_info():
-  try:
-    return _get_version_info_from_package()
-  except FileNotFoundError:
-    pass
+def _get_version_info_from_package():
+  with open(PACKAGE_VERSION_FILE) as f:
+    data = json.load(f)
+  return VersionInfo(**data)
 
-  return _get_version_info_from_git()
+
+def prepare_version_info_for_package():
+  vi = _get_version_info_from_git()
+  with open(PACKAGE_VERSION_FILE, mode='w') as f:
+    json.dump(vi._asdict(), f, ensure_ascii=True, indent=2, sort_keys=True)
+  return vi
